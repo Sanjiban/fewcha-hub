7aa18ed5757971d66966098314ed338119393193
Sanjiban Bairagya <sanjiban@sanjiban-laptop.(none)>
2012-10-04 03:40:46 +0530

    Removed 3 files

--- print_dates.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from git import *
-import time
-repo=Repo('/home/sanjiban/fewcha-hub')
-commit_list=repo.iter_commits('master')
-for my_commit in commit_list:
-	print time.asctime(time.gmtime(my_commit.committed_date))
